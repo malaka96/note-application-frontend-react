07b808fd3a6cea596c1fa7c1530fd76fe8e671e9
malaka96 <madhubhashana655@gmail.com>
2025-12-22 16:53:57 +0530

    nav bar added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './componets/Navbar';
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <h1 className='' >Yello malaka</h1>
+    </>
+    
+  );
+}
+
+export default App
