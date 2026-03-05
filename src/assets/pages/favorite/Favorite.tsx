896d78b801a074320b7f4438872c5ff757c3a693
malaka96 <madhubhashana655@gmail.com>
2026-03-05 23:11:52 +0530

    completed delete method of favorite notes

--- src/assets/pages/favorite/Favorite.tsx
+++ src/assets/pages/favorite/Favorite.tsx
@@ -1,23 +1,36 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { NoteContext } from "../../../context/NoteContext";
 import NoteCard from "../../../componets/NoteCard";
+import { deleteNote } from "../../../api/NoteApi";
 
 const Favorite = () => {
+  const { favoriteNotes, setNotes, setFavoriteNotes } = useContext(NoteContext)!;
 
-  const {favoriteNotes} = useContext(NoteContext)!;
-
-  async function handleDelete(id:number) {
-
+  async function handleDelete(id: number) {
+    try {
+      const res = await deleteNote(id);
+      if (res.status === 200) {
+        setNotes((prev) => prev.filter((note) => note.id !== id));
+        setFavoriteNotes((prev) => prev.filter((note) => note.id !== id));
+        console.log("note deleted");
+      } else {
+        console.log(res.status);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <div className="p-8 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
       {favoriteNotes && favoriteNotes.length > 0 ? (
         favoriteNotes.map((note) => (
-          <NoteCard key={note.id} note={note} handleDelete={handleDelete}/>
+          <NoteCard key={note.id} note={note} handleDelete={handleDelete} />
         ))
       ) : (
-        <p className="text-center text-gray-500 col-span-full">No favorite notes yet</p>
+        <p className="text-center text-gray-500 col-span-full">
+          No favorite notes yet
+        </p>
       )}
     </div>
   );
